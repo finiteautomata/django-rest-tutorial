b9110a7b1c20fb1b40d390c7a091cba0cd23a8de
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-09-02 17:23:44 -0300

    Part 1 finished

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -21,11 +21,11 @@ from quickstart import views
 
 router = routers.DefaultRouter()
 
-router.register('users', views.UserViewSet)
-router.register('groups', views.GroupViewSet)
+#router.register('users', views.UserViewSet)
+#router.register('groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('admin/', admin.site.urls),
+    path('', include('snippets.urls')),
+    #path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    #path('admin/', admin.site.urls),
 ]
