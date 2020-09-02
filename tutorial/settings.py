8bcdd7864ff5456a77ea4c03851a84d9d31b2efb
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-09-02 16:25:18 -0300

    Add snippets

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    # OBS: it says I should add snippets.apps.SnippetsConfig
+    'snippets',
+
 ]
 
 MIDDLEWARE = [
